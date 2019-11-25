41441aff33b83cb67cb570e10f19f9a92285c785
matsuura takahito <ku.takahito058@gmail.com>
2019-11-25 16:42:41 +0900

    tweak phase

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ $ git clone https://github.com/takapon564/todo-api.git
 
 $ cd todo-api
 
-$ pip3 install flask flask-restful flask-sqlalchemy 
+$ pip3 install flask flask-restful flask-sqlalchemy pymysql 
 
 ```  
 
---
+++ production_app.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask
+from flask_restful import Resource, Api
+from resources.todo import Todo, TodoList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@10.0.30.6/test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+api = Api(app)
+
+
+api.add_resource(Todo, '/todos/<string:title>')
+api.add_resource(TodoList, '/todos')
+
+if __name__ == "__main__":
+    from db import db
+    db.init_app(app)
+
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000)
